f7db591747dc04b3375860bee91b9afdf8a75d6e
Grzegorz Biesiadecki <gbiesiadecki@gmail.com>
2014-09-17 21:48:05 -0700

    Fixed code

--- src/alvin_watch/core.clj
+++ src/alvin_watch/core.clj
@@ -1,39 +1,56 @@
 (ns alvin-watch.core
   (:require [clj-http.client :as client]
             [watchtower.core])
-  (:use [watchtower.core]))
+
+  (:use [watchtower.core]
+        [twitter.oauth]
+        [twitter.callbacks]
+        [twitter.callbacks.handlers]
+        [twitter.api.restful]))
 
 
-(defn moisture []
-  (let
-    [data (slurp "./data/moisture.txt")]
+(def moisture-file "/home/pi/projects/RF24/RPi/RF24/examples/moisture.txt")
+; (def moisture-file "./data/moisture.txt")
 
-    (cond
-      (= data "") 0
-      :else (Integer. (read-string (slurp "./data/moisture.txt"))))))
+(load "oauth-keys")
 
-
-(def old-moisture (moisture))
+(def oauth-creds (make-oauth-creds consumer-key
+                                   consumer-secret
+                                   oauth-token
+                                   oauth-token-secret))
 
 
 (defn delta [old, new]
   (Math/abs (- old new)))
 
 
+(defn moisture []
+  (let
+    [data (slurp moisture-file)]
+
+    (cond
+      (= data "") 0
+      :else (Integer. (read-string (slurp moisture-file))))))
+
+
+(def old-moisture (atom (moisture)))
+
+
 (defn send-message [value]
-  (println "changed" value))
+  (println (str "Moisture: " value "%"))
+  (statuses-update :oauth-creds oauth-creds :params { :status (str "Moisture: " value "%") }))
 
 
 (defn on-moisture-change [file]
-  (send-message (moisture)))
-  ; (cond
-    ; (> (delta old-moisture (moisture)) 10) (send-message)
-    ; :else ""))
+  (if (> (delta @old-moisture (moisture)) 10)
+    (do
+      (send-message (moisture))
+      (reset! old-moisture (moisture)))))
 
 
 (defn -main [& args]
   (println "Watching for moisture changes...")
 
-  (watcher ["./data/moisture.txt"]
+  (watcher [moisture-file]
     (rate 100)
     (on-change on-moisture-change)))
