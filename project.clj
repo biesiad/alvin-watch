0e58f936bb61e1b722d741f27a25beb4d4cfa3c1
Grzegorz Biesiadecki <gbiesiadecki@gmail.com>
2014-09-17 21:47:45 -0700

    Removed unused libs

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [watchtower "0.1.1"]
-                 [clj-http "1.0.0"]]
+                 [twitter-api "0.7.6"]]
   :main alvin-watch.core)
